59d604b402d42a9049ab543d19d658f9ad565816
Nitinkumar Yadav <126796871+Jackdv67@users.noreply.github.com>
2023-05-24 07:20:12 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+// import Task1 from './Component/Task1';
+import Task3 from './Component/movie-recom/Task3';
+
+function App(){
+
+  return(
+    <div className='App-header'>
+        {/* <Task1/> */}
+        <Task3/>
+    </div>
+  )
+}
+export default App;
