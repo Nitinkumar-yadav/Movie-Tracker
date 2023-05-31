aca82d3ebe5ff59a1774aebc25d6e5b9c7f6984d
Nitinkumar Yadav <126796871+Jackdv67@users.noreply.github.com>
2023-05-31 09:53:40 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
-// import Task1 from './Component/Task1';
 import Task3 from './Component/movie-recom/Task3';
 
 function App(){
 
   return(
     <div className='App-header'>
-        {/* <Task1/> */}
         <Task3/>
     </div>
   )
